9570e2e860ef96f616560e0bee171c8463d53c63
Thiago Leite <leite.tjs@gmail.com>
2021-02-24 19:29:26 -0300

    programa positivoOuNegativo e triangulo

---
+++ bloco_4/positivoOuNegativo.js
@@ -0,0 +1,9 @@
+let numero = 0;
+
+if (numero > 0) {
+    console.log('O número é positivo');
+} else if (numero < 0) {
+    console.log('O número é negativo');
+} else {
+    console.log('O número é caso contrário')
+}
---
+++ bloco_4/triangulo.js
@@ -0,0 +1,9 @@
+let ang1 = 45;
+let ang2 = 45;
+let ang3 = 90;
+
+if (ang1 + ang2 + ang3 == 180) {
+    console.log('true');
+} else {
+    console.log('false');
+}
