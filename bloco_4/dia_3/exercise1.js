f94b743dd6d272487e1882277a2cceaf40841518
Thiago Leite <leite.tjs@gmail.com>
2021-02-26 18:37:30 -0300

    primeiro exercicio 4.2

---
+++ bloco_4/dia_3/exercise1.js
@@ -0,0 +1,13 @@
+let n = 5;
+let symbol = '*';
+let inputline = '';
+
+for (lineIndex = 0; lineIndex < n; lineIndex +=1){
+    inputline = inputline + symbol;
+}
+
+for (lineIndex = 0; lineIndex < n; lineIndex +=1){
+    console.log(inputline)
+}
+
+
