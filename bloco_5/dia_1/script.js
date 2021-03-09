f65fb2790f121144ac7dbe1cb72774c989e159f9
Thiago Leite <leite.tjs@gmail.com>
2021-03-08 22:16:42 -0300

    to fazendo no dia 4, mas sao exercicios do dia 1

---
+++ bloco_5/dia_1/script.js
@@ -0,0 +1,8 @@
+document.querySelector('#header-container').style.backgroundColor = 'lightgreen';
+document.querySelector('.emergency-tasks').style.backgroundColor = 'indianred';
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow';
+document.querySelectorAll('h3')[0].style.backgroundColor = 'purple';
+document.querySelectorAll('h3')[1].style.backgroundColor = 'purple';
+document.querySelectorAll('h3')[2].style.backgroundColor = 'black';
+document.querySelectorAll('h3')[3].style.backgroundColor = 'black';
+document.querySelectorAll('footer')[0].style.backgroundColor = 'darkslategray';
